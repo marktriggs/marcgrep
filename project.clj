6efbfe6c96eaf4b107af1a1e33a2247623db4ef1
Mark Triggs <mark@dishevelled.net>
2013-05-17 13:16:27 +1000

    Modifications to make the build work with the latest version of lein

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
-(defproject marcgrep "1.0.1"
+(defproject marcgrep "1.0.2"
   :description "A slow-moving search for MARC data"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure.contrib/complete "1.3.0-alpha4"]
                  [org.mortbay.jetty/jetty "6.1.25"]
                  [compojure "0.6.4"]
+                 [ring/ring-servlet "0.3.10"]
                  [org.tigris/marc4j "2.4"]
                  [org.apache.lucene/lucene-core "3.5.0"]]
+  :plugins [[lein-ring "0.8.5"]]
   :warn-on-reflection false
-  :dev-dependencies [[swank-clojure/swank-clojure "1.3.2"]
-                     [lein-ring "0.4.6"]]
+  :dev-dependencies [[swank-clojure/swank-clojure "1.3.2"]]
 
   ;; For producing a war file
   :ring {:handler marcgrep.core/handler
